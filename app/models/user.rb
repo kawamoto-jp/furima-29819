ac74c9c2781f854bade18680fbcf7c9d41764406
kawamoto-jp <k400345@gmail.com>
2020-09-10 11:39:58 +0900

    ユーザー管理機能　修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,19 @@ class User < ApplicationRecord
   has_many :items
   has_many :buyings
 
+  
+
   with_options presence: true do
+    writing = /\A[ぁ-んァ-ン一-龥]+\z/
+    reading = /\A[ァ-ヶー－]+\z/
+
     validates :nickname
     validates :birthday
     validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     validates :password, length: {minimum: 6}, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
-    validates :last_name_reading, :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :last_name, format: { with: writing}
+    validates :first_name, format: { with: writing}
+    validates :last_name_reading, format: { with: reading}
+    validates :first_name_reading, format: { with: reading}
   end
 end
